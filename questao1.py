9dcc52bbd9da40a94e3bb18bb78bad600702e1bb
dem19 <40030052+dem19@users.noreply.github.com>
2018-06-07 10:54:23 -0300

    Update questao1.py

--- questao1.py
+++ questao1.py
@@ -1,2 +1,29 @@
+n1 = int(input("Digite um numero"))
+n2 = int(input("Digite um numero"))
+n3 = int(input("Digite um numero"))
 
-y
+if n1 < n2 < n3:
+    print("Ordem Crescente", n1,n2,n3)
+if n1 < n3 < n2:
+    print("Ordem Crescente", n1, n3, n2)
+if n2 < n1 < n3:
+    print("Ordem Crescente", n2,n1,n3)
+if n2 < n3 < n1:
+    print("Ordem Crescente", n2,n3,n1)
+if n3 < n1 < n2:
+    print("Ordem Crescente", n3,n1,n2)
+if n3 < n2 < n1:
+    print("Ordem Crescente", n3,n2,n1)
+
+if n1 > n2 > n3:
+    print("Ordem Decrescente", n1,n2,n3)
+if n1 > n3 > n2:
+    print("Ordem Decrescente", n1, n3, n2)
+if n2 > n1 > n3:
+    print("Ordem Decrescente", n2,n1,n3)
+if n2 > n3 > n1:
+    print("Ordem Decrescente", n2,n3,n1)
+if n3 > n1 > n2:
+    print("Ordem Decrescente", n3,n1,n2)
+if n3 > n2 > n1:
+    print("Ordem Decrescente", n3,n2,n1)

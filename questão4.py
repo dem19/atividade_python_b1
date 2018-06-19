aad6323009843e5e57c4de3523d87fb838982e8b
dem19 <40030052+dem19@users.noreply.github.com>
2018-06-19 09:10:02 -0300

    Update questão4.py

--- questão4.py
+++ questão4.py
@@ -7,7 +7,7 @@ if (l1 + l2 > l3) and (l1+l3>l2) and (l2+l3>l1):
     if l1 == l2== l3 ==l1:
         print('Triângulo Equilátero')
 
-    if (l1==l3!=l2) and (l2==l1!=l3) and (l3==l2!=l1):
+    if (l1==l3!=l2) or (l2==l1!=l3) or (l3==l2!=l1):
         print('É um Triângulo Isósceles')
 
     if (l1 != l2 != l3 != l1):

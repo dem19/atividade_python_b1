f32d3981a8e970701368e21fa8192deb605fe7d2
dem19 <40030052+dem19@users.noreply.github.com>
2018-06-19 08:47:52 -0300

    Update questão5.py

--- questão5.py
+++ questão5.py
@@ -5,31 +5,31 @@ print("D – Divisão")
 print("M – Multiplicação")
 
 operação = input("Digite uma Operação:")
-c1 = int(input("Digite um Numero:"))
-c2 = int(input("Digite um Numero:"))
+num1 = int(input("Digite um Numero:"))
+num2 = int(input("Digite um Numero:"))
 
 if operação == "A":
-    print(c1+c2)
+    print(num1+num2)
 if operação == "a":
-    print(c1+c2)
+    print(num1+num2)
 
 if operação == "S":
-    print(c1-c2)
+    print(num1-num2)
 if operação == "s":
-    print(c1-c2)
+    print(num1 - num2)
 
 if operação == "D":
-    if c2 ==0:
+    if num2 ==0:
         print("erro")
-    if c2 !=0:
-        print(c1 / c2)
+    if num2 !=0:
+        print(num1 / num2)
 if operação == "d":
-        if c2 == 0:
+        if num2 == 0:
             print("erro")
-        if c2 != 0:
-            print(c1 / c2)
+        if num2 != 0:
+            print(num1 / num2)
 
 if operação == "M":
-    print(c1*c2)
+    print(num1*num2)
 if operação == "m":
-    print(c1*c2)
+    print(num1*num2)
